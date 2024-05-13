582e3477a26db84b54877803505a8ede75e4ed76
Matthew Ayres <73623288+Tirigan@users.noreply.github.com>
2024-05-13 00:09:26 -0400

    Update and rename keylime-ingredients-sourcecode.txt to keylime.py
    correcting name for sourcecode

--- keylime.py
+++ keylime.py
@@ -40,20 +40,3 @@ with open('encrypted_keystrokes', 'wb') as f:
 with open('encryption_key.txt', 'wb') as f:
     f.write(encryption_key)
 
-
-INGREDIENTS.PY
-
-from cryptography.fernet import Fernet
-
-with open('encryption_key.txt', 'rb') as f:
-    encryption_key = f.read()
-
-cipher_suite = Fernet(encryption_key)
-
-with open('encrypted_keystrokes', 'rb') as f:
-    encrypted_data = f.read()
-
-decrypted_data = cipher_suite.decrypt(encrypted_data)
-
-with open('decrypted_keystrokes.txt', 'wb') as f:
-    f.write(decrypted_data)
